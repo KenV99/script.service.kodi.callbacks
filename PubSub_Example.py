9489b449cb7c2c0a967a4149f8a74adc603d8c5d
KenV99 <KenV99@users.noreply.github.com>
2015-12-24 10:42:59 -0500

    Initial move to pubsub model

---
+++ PubSub_Example.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2015 KenV99
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import threading
+import time
+
+import PubSub_Threaded
+
+class MockPublisher(PubSub_Threaded.Publisher, threading.Thread):
+    def __init__(self, dispatcher, topic, interval=1):
+        PubSub_Threaded.Publisher.__init__(self, dispatcher)
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.abort_evt = threading.Event()
+        self.topic = topic
+
+    def run(self):
+        count = 1
+        while not self.abort_evt.is_set():
+            time.sleep(self.interval)
+            msg = PubSub_Threaded.Message(self.topic, count=count)
+            self.publish(msg)
+            count += 1
+
+
+class PrintTask(PubSub_Threaded.Task):
+    def run(self):
+        print 'Message Received %s: %s' % (self.topic.topic, str(self.kwargs))
+
+
+if __name__ == '__main__':
+    dispatch = PubSub_Threaded.Dispatcher()
+    topics = [['test_topic1', 1], ['test_topic2', 2], ['test_topic3', 3]]
+    subs = []
+    pubs = []
+    for t in topics:
+        try:
+            task = PrintTask  # NOTE: Not an instance
+            tm = PubSub_Threaded.TaskManager(task, max_runs=3)
+            subscriber = PubSub_Threaded.Subscriber()
+            subscriber.addTaskManager(tm)
+            subscriber.addTopic(PubSub_Threaded.Topic(t[0]))
+            dispatch.addSubscriber(subscriber)
+            subs.append(subscriber)
+            pub = MockPublisher(dispatch, PubSub_Threaded.Topic(t[0]), interval=t[1])
+            pubs.append(pub)
+        except Exception as e:
+            pass
+    dispatch.start()
+    for pub in pubs:
+        pub.start()
+    while True:
+        pass
+
---
+++ PubSub_Threaded.py
@@ -0,0 +1,267 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2015 KenV99
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import Queue
+import abc
+import copy
+import threading
+import time
+
+LOGLEVEL_CRITICAL = 50
+LOGLEVEL_ERROR = 40
+LOGLEVEL_WARNING = 30
+LOGLEVEL_INFO = 20
+LOGLEVEL_DEBUG = 10
+
+
+class BaseLogger(object):
+    __metaclass__ = abc.ABCMeta
+    selfloglevel = LOGLEVEL_INFO
+
+    @staticmethod
+    def setLogLevel(loglevel):
+        BaseLogger.selfloglevel = loglevel
+
+    @staticmethod
+    @abc.abstractmethod
+    def log(*args):
+        pass
+
+
+class PrintLogger(BaseLogger):
+    @staticmethod
+    def log(loglevel=LOGLEVEL_INFO, msg='Log Event'):
+        if loglevel >= BaseLogger.selfloglevel:
+            print msg
+
+
+class DummyLogger(BaseLogger):
+    @staticmethod
+    def log(*args):
+        pass
+
+
+class Topic(object):
+    def __init__(self, topic, subtopic=None):
+        self.topic = topic
+        self.subtopic = subtopic
+
+    def has_subtopic(self):
+        if self.subtopic is None:
+            return False
+        else:
+            return True
+
+    def __eq__(self, other):
+        assert isinstance(other, Topic)
+        if other.has_subtopic():
+            if self.has_subtopic():
+                if other.topic == self.topic and other.subtopic == self.subtopic:
+                    return True
+                else:
+                    return False
+            else:  ## self has no subtopic
+                return False
+        else:
+            if self.has_subtopic():
+                if self.topic == other.topic:
+                    return True
+                else:
+                    return False
+            else:
+                if self.topic == other.topic:
+                    return True
+                else:
+                    return False
+
+    def __repr__(self):
+        if self.has_subtopic():
+            return '%s:%s' % (self.topic, self.subtopic)
+        else:
+            return self.topic
+
+
+class Message(object):
+    def __init__(self, topic, **kwargs):
+        self.topic = topic
+        self.kwargs = kwargs
+
+
+class Dispatcher(threading.Thread):
+    def __init__(self, interval=0.1, sleepfxn=time.sleep):
+        super(Dispatcher, self).__init__()
+        self._message_q = Queue.Queue()
+        self._abort_evt = threading.Event()
+        self._abort_evt.clear()
+        self.subscribers = []
+        self.running = False
+        self.interval = interval
+        self.sleepfxn = sleepfxn
+
+    def addSubscriber(self, subscriber):
+        assert isinstance(subscriber, Subscriber)
+        wasrunning = False
+        if self.running:
+            self.abort()
+            self.join()
+            wasrunning = True
+        self.subscribers.append(subscriber)
+        if wasrunning:
+            self.start()
+
+    def q_message(self, message):
+        self._message_q.put(copy.copy(message), block=False)
+
+    def run(self):
+        self.running = True
+        while not self._abort_evt.is_set():
+            while not self._message_q.empty():
+                try:
+                    msg = self._message_q.get_nowait()
+                    assert isinstance(msg, Message)
+                except Queue.Empty:
+                    continue
+                except AssertionError:
+                    raise
+                for s in self.subscribers:
+                    if msg.topic in s.topics:
+                        s.notify(msg)
+            self.sleepfxn(self.interval)
+        self.running = False
+
+    def abort(self):
+        if self.running:
+            self._abort_evt.set()
+
+
+class Publisher(object):
+    def __init__(self, dispatcher):
+        self.dispatcher = dispatcher
+
+    def publish(self, message):
+        self.dispatcher.q_message(message)
+
+
+class Task(threading.Thread):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        super(Task, self).__init__()
+        self.kwargs = ()
+        self.topic = None
+
+    def t_start(self, topic, **kwargs):
+        try:
+            self.kwargs = kwargs
+            self.topic = topic
+        except:
+            pass
+        self.start()
+
+    @abc.abstractmethod
+    def run(self):
+        pass
+
+
+class TaskManager(object):
+    def __init__(self, task, maxrunning=1, refractory_period=None, max_runs=-1):
+        self.task = task
+        self.maxrunning = maxrunning
+        self.refractory_period = refractory_period
+        self.run_tasks = []
+        self.most_recent_task_time = time.time()
+        self.max_runs = max_runs
+        self.run_count = 0
+
+    def start(self, topic, **kwargs):
+        if self.max_runs > 0:
+            if self.run_count >= self.max_runs:
+                raise TaskManagerException_TaskCountExceeded
+        if self.maxrunning > 0:
+            count = 0
+            for i, t in enumerate(self.run_tasks):
+                assert isinstance(t, threading.Thread)
+                if t.is_alive():
+                    count += 1
+                else:
+                    del t
+                    del self.run_tasks[i]
+            if count >= self.maxrunning:
+                raise TaskManagerException_TaskAlreadyRunning
+        if self.refractory_period is not None:
+            tnow = time.time()
+            if tnow - self.most_recent_task_time < self.refractory_period:
+                raise TaskManagerException_TaskInRefractoryPeriod
+            else:
+                self.most_recent_task_time = tnow
+
+        # Launch the task
+        self.run_count += 1
+        t = self.task()
+        t.t_start(topic, **kwargs)
+        if self.maxrunning > 0:
+            self.run_tasks.append(t)
+
+
+class TaskManagerException_TaskCountExceeded(Exception):
+    def __init__(self):
+        super(TaskManagerException_TaskCountExceeded, self).__init__('Task not run because task count exceeded')
+
+
+class TaskManagerException_TaskAlreadyRunning(Exception):
+    def __init__(self):
+        super(TaskManagerException_TaskAlreadyRunning, self).__init__('Task not run because task already running')
+
+
+class TaskManagerException_TaskInRefractoryPeriod(Exception):
+    def __init__(self):
+        super(TaskManagerException_TaskInRefractoryPeriod, self).__init__(
+                'Task not run because task is in refractory period')
+
+
+class Subscriber(object):
+    def __init__(self, logger=DummyLogger, loglevel=LOGLEVEL_INFO):
+        self.topics = []
+        self.taskmanagers = []
+        self.logger = logger
+        self.loglevel = loglevel
+
+    def addTopic(self, topic):
+        assert isinstance(topic, Topic)
+        self.topics.append(topic)
+
+    def addTaskManager(self, tm):
+        assert isinstance(tm, TaskManager)
+        self.taskmanagers.append(tm)
+
+    def notify(self, message):
+        # assert isinstance(message, Message)
+        for taskmanager in self.taskmanagers:
+            try:
+                taskmanager.start(message.topic, **message.kwargs)
+            except TaskManagerException_TaskAlreadyRunning as e:
+                self.logger.log(self.loglevel, '%s - %s' % (message.topic, e.message))
+            except TaskManagerException_TaskInRefractoryPeriod as e:
+                self.logger.log(self.loglevel, '%s - %s' % (message.topic, e.message))
+            except TaskManagerException_TaskCountExceeded as e:
+                self.logger.log(self.loglevel, '%s - %s' % (message.topic, e.message))
+            except Exception as e:
+                pass
+            else:
+                self.logger.log(self.loglevel, 'Task launched for %s' % message.topic)
